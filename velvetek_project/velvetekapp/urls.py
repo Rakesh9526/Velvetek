3800bfe57f02892b74f71f158a79a597fe2ca651
nijiya <nijiyanijzz@gmail.com>
2024-12-28 10:41:25 +0530

    customer page created

--- velvetek_project/velvetekapp/urls.py
+++ velvetek_project/velvetekapp/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.adminhome,name='adminhome')
+    path('',views.adminhome,name='adminhome'),
+    path('add_customer',views.add_customer,name='add_customer'),
+    path('new_customer',views.new_customer,name='new_customer')
 ]
